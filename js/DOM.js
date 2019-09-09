b010c68f6088353d7e17b1d9feade7e16f73c52c
yusfi1997 <yusfiadil@gmail.com>
2019-09-09 14:36:19 +0700

    hallo

---
+++ js/DOM.js
@@ -0,0 +1,3 @@
+function tentangsaya() {
+    document.getElementById("tentang").innerHTML = " Nama Saya Yusfi Adil Amrullah. Lahir Di Jember, Jawa Timur pada tanggal 11 Oktober 1997. Saya memiliki kondisi kesehatan yang baik, loyalitas tinggi, jujur, ulet, cepat memahami bidang baru yang sedang dipahami dan dikerjakan.";
+}
